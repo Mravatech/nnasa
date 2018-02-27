6e0a6a6aba8663bcc29dca254be9387a84dee1d1
okli <okli@isddesign.com>
2018-02-27 09:56:24 +0200

    move logic to crop activity

--- app/src/main/java/com/mnassa/other/CropActivity.kt
+++ app/src/main/java/com/mnassa/other/CropActivity.kt
@@ -1,9 +1,11 @@
 package com.mnassa.other
 
 import android.app.Activity
+import android.content.ContentValues
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
 import com.yalantis.ucrop.UCrop
 import java.io.File
@@ -13,6 +15,7 @@ import java.io.File
  * User: okli
  * Date: 2/26/2018
  */
+
 class CropActivity : AppCompatActivity() {
 
     private var mUri: Uri? = null
@@ -21,38 +24,102 @@ class CropActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        mUri = intent.getParcelableExtra(URI_PHOTO)
-
         val filename = System.currentTimeMillis().toString() + ".jpg"
         cachedUri = Uri.fromFile(File(cacheDir.toString() + "/" + filename))
 
-        UCrop.of(mUri!!, cachedUri!!)
-                .start(this)
+        getPhoto(intent.getIntExtra(PHOTO_INTENT_FLAG, 0))
+
+    }
+
+
+    private fun getPhoto(flag: Int) {
+        when (flag) {
+            REQUEST_CODE_GALLERY -> requestPhotoGallery()
+            REQUEST_CODE_CAMERA -> requestCamera()
+            else -> {
+                setResult(GET_PHOTO_ERROR)
+                finish()
+            }
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == RESULT_OK && requestCode == UCrop.REQUEST_CROP) {
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE_CAMERA) {
+            mUri = if (data != null) data.data else mUri
+            cropImage()
+        } else if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE_GALLERY) {
+            mUri = data?.data
+            cropImage()
+        } else if (resultCode == RESULT_CANCELED) {
+            setResult(GET_PHOTO_ERROR)
+            finish()
+        } else if (resultCode == RESULT_OK && requestCode == UCrop.REQUEST_CROP) {
             setResult(RESULT_OK, intent.putExtra(URI_PHOTO_RESULT, cachedUri))
             finish()
         } else if (resultCode == UCrop.RESULT_ERROR) {
-            setResult(CROP_PHOTO_ERROR)
+            setResult(GET_PHOTO_ERROR)
             finish()
         }
-
     }
 
+    private fun cropImage() {
+        if (mUri != null) {
+            UCrop.of(mUri!!, cachedUri!!)
+                    .start(this)
+        }
+    }
+
+    private fun requestCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+        if (intent.resolveActivity(packageManager) != null) {
+            val values = ContentValues()
+            values.put(MediaStore.Images.Media.TITLE, System.currentTimeMillis())
+            values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis())
+            mUri = contentResolver?.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, mUri)
+            startActivityForResult(intent, REQUEST_CODE_CAMERA)
+        } else {
+            setResult(GET_PHOTO_ERROR)
+            finish()
+        }
+    }
+
+    private fun requestPhotoGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, getIntent().getBooleanExtra(Intent.EXTRA_ALLOW_MULTIPLE, false))
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivityForResult(intent, REQUEST_CODE_GALLERY)
+        } else {
+            setResult(GET_PHOTO_ERROR)
+            finish()
+        }
+    }
+    // delete if above method works well
+//    private fun pickFromGallery() {
+//        val intent = Intent()
+//        intent.type = "image/*"
+//        intent.action = Intent.ACTION_GET_CONTENT
+//        intent.addCategory(Intent.CATEGORY_OPENABLE)
+//        // put to resources
+//        startActivityForResult(Intent.createChooser(intent, "Select photo"), REQUEST_CODE_PICTURE)
+//    }
+
+
     companion object {
-        private const val URI_PHOTO = "URI_PHOTO"
+        private const val PHOTO_INTENT_FLAG = "PHOTO_INTENT_FLAG"
         const val URI_PHOTO_RESULT = "URI_PHOTO_RESULT"
-        const val CROP_PHOTO_ERROR = 102
-        const val CROP_PHOTO_CODE = 103
+        const val GET_PHOTO_ERROR = 102
+        const val REQUEST_CODE_GALLERY = 1
+        const val REQUEST_CODE_CAMERA = 2
 
-        fun start(uri: Uri, activity: Activity) : Intent {
+        fun start(flag: Int, activity: Activity): Intent {
             val intent = Intent(activity, CropActivity::class.java)
-            intent.putExtra(URI_PHOTO, uri)
+            intent.putExtra(PHOTO_INTENT_FLAG, flag)
             return intent
-
         }
     }
 
--- app/src/main/java/com/mnassa/screen/profile/ProfileController.kt
+++ app/src/main/java/com/mnassa/screen/profile/ProfileController.kt
@@ -1,9 +1,6 @@
 package com.mnassa.screen.profile
 
 import android.app.Activity
-import android.content.ContentValues
-import android.content.Intent
-import android.provider.MediaStore
 import android.view.View
 import com.bumptech.glide.Glide
 import com.github.salomonbrys.kodein.instance
@@ -12,7 +9,9 @@ import com.mnassa.core.addons.launchCoroutineUI
 import com.mnassa.screen.base.MnassaControllerImpl
 import kotlinx.android.synthetic.main.controller_crop.view.*
 import android.net.Uri
+import android.support.annotation.IntRange
 import com.mnassa.other.CropActivity
+import timber.log.Timber
 import java.io.File
 
 /**
@@ -36,70 +35,30 @@ class ProfileController : MnassaControllerImpl<ProfileViewModel>() {
             //set text from resources
             btnTakePhotoFromCamera.text = "Camera"
             btnTakePhotoFromCamera.setOnClickListener {
-                requestCamera()
+                startCropActivity(CropActivity.REQUEST_CODE_CAMERA)
             }
 
             btnTakePhotoFromGallery.text = "Gallery"
             btnTakePhotoFromGallery.setOnClickListener {
-                pickFromGallery()
+                startCropActivity(CropActivity.REQUEST_CODE_GALLERY)
             }
-            btnCrop.text = "Crop Photo"
-            btnCrop.setOnClickListener {
-                cropImage()
-            }
-
         }
 
         launchCoroutineUI {
             onActivityResult.subscribe {
-
-                if (it.resultCode == Activity.RESULT_OK && it.requestCode == REQUEST_CODE_CAMERA) {
-                    mUri = if (it.data != null) it.data?.data else mUri
+                if (it.resultCode == Activity.RESULT_OK && it.requestCode == REQUEST_CODE_CROP) {
+                    mUri = it.data?.getParcelableExtra(CropActivity.URI_PHOTO_RESULT) ?: mUri!!
                     setImage(mUri)
-                } else if (it.resultCode == Activity.RESULT_OK && it.requestCode == REQUEST_CODE_PICTURE) {
-                    mUri = it.data?.data
-                    setImage(mUri)
-                } else if (it.resultCode == Activity.RESULT_OK && it.requestCode == CropActivity.CROP_PHOTO_CODE) {
-                    mUri = it.data?.getParcelableExtra(CropActivity.URI_PHOTO_RESULT)
-                            ?: mUri!!
-                    setImage(mUri)
-                } else if (it.resultCode == CropActivity.CROP_PHOTO_ERROR) {
-                    //handle here
+                } else if (it.resultCode == CropActivity.GET_PHOTO_ERROR) {
+                    Timber.i("CropActivity.GET_PHOTO_ERROR")
                 }
             }
         }
-
     }
 
-    private fun cropImage() {
-        if (mUri != null) {
-            startActivityForResult(CropActivity.start(mUri!!, activity!!), CropActivity.CROP_PHOTO_CODE)
-        }
-    }
-
-    private fun requestCamera() {
-        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
-        if (intent.resolveActivity(activity?.packageManager) != null) {
-            val values = ContentValues()
-            values.put(MediaStore.Images.Media.TITLE, System.currentTimeMillis())
-            values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis())
-            mUri = activity?.contentResolver?.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
-
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, mUri)
-            startActivityForResult(intent, REQUEST_CODE_CAMERA)
-        } else {
-            // handle
-        }
-    }
-
-    private fun pickFromGallery() {
-        val intent = Intent()
-        intent.type = "image/*"
-        intent.action = Intent.ACTION_GET_CONTENT
-        intent.addCategory(Intent.CATEGORY_OPENABLE)
-        // put to resources
-        startActivityForResult(Intent.createChooser(intent, "Select photo"), REQUEST_CODE_PICTURE)
+    private fun startCropActivity(@IntRange(from = 1, to = 2) flag: Int) {
+        val intent = CropActivity.start(flag, activity!!)
+        startActivityForResult(intent, REQUEST_CODE_CROP)
     }
 
     private fun setImage(resultUri: Uri?) {
@@ -109,8 +68,7 @@ class ProfileController : MnassaControllerImpl<ProfileViewModel>() {
     }
 
     companion object {
-        private const val REQUEST_CODE_PICTURE = 1
-        private const val REQUEST_CODE_CAMERA = 2
+        private const val REQUEST_CODE_CROP = 101
 
         fun newInstance() = ProfileController()
     }
