59a39f9ab448a9ac18abc7fcb6c825f95abc1252
okli <okli@isddesign.com>
2018-03-05 19:37:39 +0200

    location by text was added

---
+++ app/src/main/java/com/mnassa/screen/registration/PlaceAutocompleteAdapter.kt
@@ -0,0 +1,123 @@
+package com.mnassa.screen.registration
+
+import android.content.Context
+import android.graphics.Typeface
+import android.text.style.StyleSpan
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Filter
+import android.widget.Filterable
+import android.widget.TextView
+import com.google.android.gms.location.places.AutocompletePrediction
+import com.google.android.gms.location.places.AutocompleteFilter
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.common.data.DataBufferUtils
+import android.widget.Toast
+import java.util.concurrent.TimeUnit
+import com.google.android.gms.location.places.Places
+import com.mnassa.R
+import timber.log.Timber
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: okli
+ * Date: 3/5/2018
+ */
+
+class PlaceAutocompleteAdapter(context: Context,
+                               googleApiClient: GoogleApiClient,
+                               bounds: LatLngBounds?,
+                               filter: AutocompleteFilter?)
+    : ArrayAdapter<AutocompletePrediction>(context,
+        android.R.layout.simple_expandable_list_item_2,
+        android.R.id.text1), Filterable {
+
+    private var mResultList: ArrayList<AutocompletePrediction>? = null
+    private var mGoogleApiClient: GoogleApiClient = googleApiClient
+    private var mBounds: LatLngBounds? = bounds
+    private var mPlaceFilter: AutocompleteFilter? = filter
+
+    override fun getCount(): Int {
+        return mResultList?.size ?: 0
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val row = super.getView(position, convertView, parent)
+        val item = getItem(position)
+        item?.let {
+            val textView1 = row.findViewById<TextView>(android.R.id.text1)
+            val textView2 = row.findViewById<TextView>(android.R.id.text2)
+            textView1.text = item.getPrimaryText(STYLE_BOLD)
+            textView2.text = item.getSecondaryText(STYLE_BOLD)
+        }
+        return row
+    }
+
+    override fun getItem(position: Int): AutocompletePrediction? {
+        return mResultList?.getOrNull(position)
+    }
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                val results = FilterResults()
+                val c = constraint ?: return results
+                val autocompletePredictions = getAutocomplete(c) ?: return results
+                results.values = autocompletePredictions
+                results.count = autocompletePredictions.size
+                return results
+            }
+
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+                results?.takeIf { it.count > 0 }?.apply {
+                    mResultList = results.values as ArrayList<AutocompletePrediction>?
+                    notifyDataSetChanged()
+                } ?: run {
+                    notifyDataSetInvalidated()
+                }
+            }
+
+            override fun convertResultToString(resultValue: Any): CharSequence {
+                return when (resultValue) {
+                    is AutocompletePrediction -> resultValue.getFullText(null)
+                    else -> super.convertResultToString(resultValue)
+                }
+            }
+        }
+    }
+
+    private fun getAutocomplete(constraint: CharSequence): ArrayList<AutocompletePrediction>? {
+        when (mGoogleApiClient.isConnected) {
+            true -> {
+                Timber.i("Starting autocomplete query for: $constraint")
+                val results = Places.GeoDataApi.getAutocompletePredictions(
+                        mGoogleApiClient, constraint.toString(), mBounds, mPlaceFilter)
+                val autocompletePredictions = results.await(60, TimeUnit.SECONDS)
+                val status = autocompletePredictions.status
+                return when (status.isSuccess) {
+                    false -> {
+                        Toast.makeText(context, "${context.getString(R.string.could_not_connect_google_api)} $status",
+                                Toast.LENGTH_SHORT).show()
+                        Timber.e("Error getting autocomplete prediction API call: $status")
+                        autocompletePredictions.release()
+                        null
+                    }
+                    else -> {
+                        Timber.i("Query completed. Received ${autocompletePredictions.count} predictions.")
+                        DataBufferUtils.freezeAndClose(autocompletePredictions)
+                    }
+                }
+            }
+            else -> {
+                Timber.e("Google API client is not connected for autocomplete query.")
+                return null
+            }
+        }
+    }
+
+    companion object {
+        private val STYLE_BOLD = StyleSpan(Typeface.BOLD)
+    }
+}
--- app/src/main/java/com/mnassa/screen/registration/RegistrationController.kt
+++ app/src/main/java/com/mnassa/screen/registration/RegistrationController.kt
@@ -1,16 +1,20 @@
 package com.mnassa.screen.registration
 
+import android.content.Context
+import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v4.view.PagerAdapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AutoCompleteTextView
 import com.bluelinelabs.conductor.RouterTransaction
 import com.github.salomonbrys.kodein.instance
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.places.Places
 import com.mnassa.R
 import com.mnassa.core.addons.launchCoroutineUI
 import com.mnassa.other.fromDictionary
-import com.mnassa.other.validators.*
 import com.mnassa.screen.accountinfo.organization.OrganizationInfoController
 import com.mnassa.screen.accountinfo.personal.PersonalInfoController
 import com.mnassa.screen.base.MnassaControllerImpl
@@ -22,20 +26,26 @@ import kotlinx.coroutines.experimental.channels.consumeEach
 /**
  * Created by Peter on 2/26/2018.
  */
-class RegistrationController : MnassaControllerImpl<RegistrationViewModel>() {
+class RegistrationController : MnassaControllerImpl<RegistrationViewModel>(), GoogleApiClient.ConnectionCallbacks {
     override val layoutId: Int = R.layout.controller_registration
     override val viewModel: RegistrationViewModel by instance()
 
+    private lateinit var googleApiClient: GoogleApiClient
+
     override fun onViewCreated(view: View) {
         super.onViewCreated(view)
-
+        val builder = GoogleApiClient.Builder(activity!!)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .addConnectionCallbacks(this)
+        googleApiClient = builder.build()
+        googleApiClient.connect()
         with(view) {
             tvRegistrationHeader.text = fromDictionary(R.string.reg_title)
-            vpRegistration.adapter = RegistrationAdapter()
+            vpRegistration.adapter = RegistrationAdapter(view.context, googleApiClient)
             tlRegistration.setupWithViewPager(vpRegistration)
             btnNext.setOnClickListener { processRegisterClick() }
         }
-
         launchCoroutineUI {
             viewModel.openScreenChannel.consumeEach {
                 val controller = when (it) {
@@ -50,7 +60,6 @@ class RegistrationController : MnassaControllerImpl<RegistrationViewModel>() {
                 router.replaceTopController(RouterTransaction.with(controller))
             }
         }
-
         launchCoroutineUI {
             viewModel.errorMessageChannel.consumeEach {
                 Snackbar.make(view, it, Snackbar.LENGTH_SHORT).show()
@@ -58,28 +67,26 @@ class RegistrationController : MnassaControllerImpl<RegistrationViewModel>() {
         }
     }
 
+    override fun onDestroy() {
+        if (googleApiClient.isConnected) {
+            googleApiClient.disconnect()
+        }
+        super.onDestroy()
+    }
+
     private fun validatePersonInfo(): Boolean {
         val v = view
-        return v != null /*&& with(v) {
-            tilPersonFirstName.validateAsFirstName() &&
-                    tilPersonSecondName.validateAsLastName() &&
-                    tilPersonUserName.validateAsUserName() &&
-                    tilPersonCity.validateAsCity() &&
-                    tilPersonOffers.validateAsOffers() &&
-                    tilPersonInterests.validateAsInterests()
-        }*/
+        return v != null
     }
 
     private fun validateOrganizationInfo(): Boolean {
         val v = view
-        return v != null /*&& with(v) {
-            tilCompanyName.validateAsCompanyName() &&
-                    tilCompanyUserName.validateAsUserName() &&
-                    tilCompanyCity.validateAsCity() &&
-                    tilCompanyOffers.validateAsOffers() &&
-                    tilCompanyInterests.validateAsInterests()
-        }*/
+        return v != null
     }
+
+    override fun onConnected(p0: Bundle?) {}
+
+    override fun onConnectionSuspended(p0: Int) {}
 
     private fun processRegisterClick() {
         with(requireNotNull(view)) {
@@ -88,13 +95,13 @@ class RegistrationController : MnassaControllerImpl<RegistrationViewModel>() {
                         firstName = etPersonFirstName.text.toString(),
                         secondName = etPersonSecondName.text.toString(),
                         userName = etPersonUserName.text.toString(),
-                        city = etPersonCity.text.toString(),
+                        city = actvPersonCity.text.toString(),
                         offers = etPersonOffers.text.toString(),
                         interests = etPersonInterests.text.toString())
                 PAGE_ORGANIZATION_INFO -> if (validateOrganizationInfo()) viewModel.registerOrganization(
                         companyName = etCompanyName.text.toString(),
                         userName = etCompanyUserName.text.toString(),
-                        city = etCompanyCity.text.toString(),
+                        city = actvCompanyCity.text.toString(),
                         offers = etCompanyOffers.text.toString(),
                         interests = etCompanyInterests.text.toString()
                 )
@@ -110,7 +117,19 @@ class RegistrationController : MnassaControllerImpl<RegistrationViewModel>() {
         fun newInstance() = RegistrationController()
     }
 
-    class RegistrationAdapter : PagerAdapter() {
+    class RegistrationAdapter(
+            private val context: Context,
+            private val googleApiClient: GoogleApiClient)
+        : PagerAdapter(), GoogleApiClient.ConnectionCallbacks {
+
+        private lateinit var placeAutocompleteAdapter: PlaceAutocompleteAdapter
+        private var selectedPlaceId: String? = null
+        private var selectedPlaceName: String? = null
+
+        override fun onConnected(p0: Bundle?) {}
+
+        override fun onConnectionSuspended(p0: Int) {}
+
         override fun isViewFromObject(view: View, obj: Any): Boolean = view == obj
 
         override fun instantiateItem(container: ViewGroup, position: Int): Any {
@@ -119,16 +138,13 @@ class RegistrationController : MnassaControllerImpl<RegistrationViewModel>() {
                 PAGE_ORGANIZATION_INFO -> R.layout.registration_organization
                 else -> throw IllegalArgumentException("Invalid page position $position")
             }
-
             val view = LayoutInflater.from(container.context).inflate(layoutId, container, false)
             container.addView(view)
-
             when (position) {
                 PAGE_PERSON_INFO -> onPersonalPageCreated(view)
                 PAGE_ORGANIZATION_INFO -> onOrganizationPageCreated(view)
                 else -> throw IllegalArgumentException("Invalid page position $position")
             }
-
             return view
         }
 
@@ -155,6 +171,7 @@ class RegistrationController : MnassaControllerImpl<RegistrationViewModel>() {
                 tilPersonOffers.hint = "Offers"
                 tilPersonInterests.hint = "Interests"
             }
+            setAdapter(view.actvPersonCity)
         }
 
         private fun onOrganizationPageCreated(view: View) {
@@ -165,6 +182,20 @@ class RegistrationController : MnassaControllerImpl<RegistrationViewModel>() {
                 tilCompanyOffers.hint = "Offers"
                 tilCompanyInterests.hint = "Interests"
             }
+            setAdapter(view.actvCompanyCity)
         }
+
+        private fun setAdapter(city: AutoCompleteTextView) {
+            placeAutocompleteAdapter = PlaceAutocompleteAdapter(context, googleApiClient, null, null)
+            city.setAdapter(placeAutocompleteAdapter)
+            city.setOnItemClickListener({ adapterView, view1, i, l ->
+                if (placeAutocompleteAdapter.getItem(i) == null) {
+                    return@setOnItemClickListener
+                }
+                selectedPlaceId = placeAutocompleteAdapter.getItem(i)?.placeId
+                selectedPlaceName = city.text.toString()
+            })
+        }
+
     }
 }
