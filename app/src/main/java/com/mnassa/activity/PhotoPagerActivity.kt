e372c2fc540fe6c708588b50d38f4a7388d461f2
Peter <pilc@isddesign.com>
2018-03-27 13:25:00 +0300

    pull request fixes

--- app/src/main/java/com/mnassa/activity/PhotoPagerActivity.kt
+++ app/src/main/java/com/mnassa/activity/PhotoPagerActivity.kt
@@ -2,21 +2,15 @@ package com.mnassa.activity
 
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.support.v4.view.PagerAdapter
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.view.WindowManager
+import android.view.*
 import com.mnassa.R
 import com.mnassa.extensions.image
 import kotlinx.android.synthetic.main.activity_photo_pager.*
 import kotlinx.android.synthetic.main.item_image.view.*
-import android.view.ViewConfiguration
-
 
 
 /**
@@ -39,7 +33,7 @@ class PhotoPagerActivity : AppCompatActivity(), View.OnClickListener {
         pager.adapter = RegistrationAdapter(images, this)
 
         pivImages.count = images.size
-        pager.addOnPageChangeListener(object: ViewPager.SimpleOnPageChangeListener() {
+        pager.addOnPageChangeListener(object : ViewPager.SimpleOnPageChangeListener() {
             override fun onPageSelected(position: Int) {
                 pivImages.selection = position
             }
@@ -52,15 +46,13 @@ class PhotoPagerActivity : AppCompatActivity(), View.OnClickListener {
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
         if (hasFocus) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                decorView!!.systemUiVisibility =
-                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
-                        View.SYSTEM_UI_FLAG_FULLSCREEN or
-                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-            }
+            decorView?.systemUiVisibility =
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         }
     }
 
@@ -74,35 +66,31 @@ class PhotoPagerActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun hideSystemUI() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-            decorView?.systemUiVisibility =
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
-                    View.SYSTEM_UI_FLAG_FULLSCREEN or
-                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        decorView?.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
 
-            val layoutParams = pivImages.layoutParams as ViewGroup.MarginLayoutParams
-            layoutParams.bottomMargin = resources.getDimensionPixelSize(R.dimen.padding_vertical)
-            pivImages.layoutParams = layoutParams
-        }
+        val layoutParams = pivImages.layoutParams as ViewGroup.MarginLayoutParams
+        layoutParams.bottomMargin = resources.getDimensionPixelSize(R.dimen.padding_vertical)
+        pivImages.layoutParams = layoutParams
     }
 
     private fun showSystemUI() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-            decorView?.systemUiVisibility =
-                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        decorView?.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
 
-            val layoutParams = pivImages.layoutParams as ViewGroup.MarginLayoutParams
-            layoutParams.bottomMargin = getNavigationBarHeight() + resources.getDimensionPixelSize(R.dimen.padding_vertical)
-            pivImages.layoutParams = layoutParams
-        }
+        val layoutParams = pivImages.layoutParams as ViewGroup.MarginLayoutParams
+        layoutParams.bottomMargin = getNavigationBarHeight() + resources.getDimensionPixelSize(R.dimen.padding_vertical)
+        pivImages.layoutParams = layoutParams
     }
 
-    fun getNavigationBarHeight(): Int {
+    private fun getNavigationBarHeight(): Int {
         val hasMenuKey = ViewConfiguration.get(this).hasPermanentMenuKey()
         val resourceId = resources.getIdentifier("navigation_bar_height", "dimen", "android")
         return if (resourceId > 0 && !hasMenuKey) {
--- app/src/main/java/com/mnassa/dialog/DialogHelper.kt
+++ app/src/main/java/com/mnassa/dialog/DialogHelper.kt
@@ -47,7 +47,7 @@ class DialogHelper {
     }
 
     @SuppressLint("SetTextI18n")
-    fun showLoginByEmailDialog(context: Context, listener: (email: String, password: String) -> Unit) {
+    fun showLoginByEmailDebugDialog(context: Context, listener: (email: String, password: String) -> Unit) {
         if (!BuildConfig.DEBUG) return
         //!!!DEBUG ONLY!!!
         val container = LinearLayout(context)
