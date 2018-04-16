ebd2cefa67d9930986f96367736b2d05aa5fd662
okli <okli@isddesign.com>
2018-04-16 18:32:06 +0300

    some span

--- app/src/main/java/com/mnassa/screen/notifications/viewholder/NotificationHolder.kt
+++ app/src/main/java/com/mnassa/screen/notifications/viewholder/NotificationHolder.kt
@@ -35,95 +35,118 @@ class NotificationHolder(itemView: View, private val onClickListener: View.OnCli
             } else {
                 item.extra?.author?.organizationInfo?.organizationName
             }
-            itemView.tvUserName.text = name
+            itemView.tvUserName.text = name + " | " + item.type
         }
-
         Timber.i(item.type)
         with(itemView) {
             ivUserIcon.avatarRound(item.extra?.author?.avatar)
-            tvNotificationInfo.text = getTextByType(item.type, item.extra?.eventName, item.extra?.totalPrice)// + " | " + item.type
+            tvNotificationInfo.text = getTextByType(item)
             tvNotificationCame.text = item.createdAt.toTimeAgo()
             llNotificationRoot.setOnClickListener(onClickListener)
             llNotificationRoot.tag = this@NotificationHolder
         }
     }
 
-    private fun getTextByType(type: String, eventName: String?, totalPrice: String?): SpannableString {
-        var result = SpannableString("")
-        var span = SpannableString(result)
-        val baseKey = "_notification_text_key"
+    private fun getTextByType(item: NotificationModel): SpannableString {
+        val type = item.type
+        val eventName = item.extra?.eventName
+        val totalPrice = item.extra?.totalPrice
         val typeRawValue = type
+        var result: SpannableString
         when (type) {
             privateChatMessage, responseChatMessage -> {
-                result = SpannableString(fromDictionary("${baseKey}-"))
+                result = SpannableString(fromDictionary("$BASE_KEY-"))
             }
             postComment -> {
-                result = SpannableString(fromDictionary("${baseKey}_commented_post"))
+                val commented = fromDictionary("$BASE_KEY$COMMENTED_POST", COMMENTED_POST_DEFAULT_VALUE)
+                val postText = COMMENTED_POST_I_NEED + item.extra?.post?.text
+                val text = "$commented $postText"
+                result = getOneSpanText(text, postText, Color.BLACK)
             }
             connectionRequest -> {
-                result = SpannableString(fromDictionary("${baseKey}_requested_connect"))
+                result = SpannableString(fromDictionary("$BASE_KEY$REQUESTED_CONNECT", REQUESTED_CONNECT_DEFAULT_VALUE))
             }
             postRepost -> {
-                result = SpannableString(fromDictionary("${baseKey}_reposted_post"))
+                result = SpannableString(fromDictionary("$BASE_KEY$REPOSTED_POST", REPOSTED_POST_DEFAULT_VALUE))
             }
             newUserJoined -> {
-                result = SpannableString(fromDictionary("${baseKey}_become_mnassa_user"))
+                result = SpannableString(fromDictionary("$BASE_KEY$BECAME_MNASSA_USER", BECAME_MNASSA_USER_DEFAULT_VALUE))
             }
             iWasRecommended -> {
-                result = SpannableString(fromDictionary("${baseKey}_recommended_you_in_post"))
+                val recommented = fromDictionary("$BASE_KEY$RECOMMENDED_YOU_IN_POST", RECOMMENDED_YOU_IN_POST_DEFAULT_VALUE)
+                val postText = COMMENTED_POST_I_NEED + item.extra?.post?.text
+                val text = "$recommented $postText"
+                result = getOneSpanText(text, postText, Color.BLACK)
             }
             iWasRecommendedInEvent -> {
-                result = SpannableString(fromDictionary("${baseKey}_recommended_you_in_event"))
+                result = SpannableString(fromDictionary("$BASE_KEY$RECOMMENDED_YOU_IN_EVENT", RECOMMENDED_YOU_IN_EVENT_DEFAULT_VALUE))
             }
             userWasRecommended -> {
-                result = SpannableString(fromDictionary("${baseKey}_recommended_you"))
+                result = SpannableString(fromDictionary("$BASE_KEY$RECOMMENDED_YOU", RECOMMENDED_YOU_DEFAULT_VALUE))
+                result = SpannableString(fromDictionary("$BASE_KEY$FOR", FOR_DEFAULT_VALUE))
             }
             userWasRecommendedByPost -> {
-                result = SpannableString(fromDictionary("${baseKey}_announcedYou"))
+                result = SpannableString(fromDictionary("$BASE_KEY$ANNOUNCED_YOU", ANNOUNCED_YOU_DEFAULT_VALUE))
             }
             generalPostByAdmin -> {
-                result = SpannableString(fromDictionary("${baseKey}_generalPostByAdmin"))
+                result = SpannableString(fromDictionary("$BASE_KEY$GENERAL_POST_BY_ADMIN", GENERAL_POST_BY_ADMIN_DEFAULT_VALUE))
             }
             newEventAttendee -> {
-                result = SpannableString(fromDictionary("${baseKey}_newEventAttendee"))
+                result = SpannableString(fromDictionary("$BASE_KEY$NEW_EVENT_ATTENDEE", NEW_EVENT_ATTENDEE_DEFAULT_VALUE))
             }
             autoSuggestYouCanHelp -> {
-                result = SpannableString(fromDictionary("${baseKey}_youCanHelp"))
+                result = SpannableString(fromDictionary("$BASE_KEY$YOU_CAN_HELP", YOU_CAN_HELP_DEFAULT_VALUE))
             }
             newEventByAdmin -> {
-                result = SpannableString(fromDictionary("${baseKey}_inviteToEvent"))
+                result = SpannableString(fromDictionary("$BASE_KEY$INVITE_TO_EVENT", INVITE_TO_EVENT_DEFAULT_VALUE))
             }
             invitesNumberChanged -> {
-                result = SpannableString(fromDictionary("${baseKey}_numberOfInvitations"))
+                result = SpannableString(fromDictionary("$BASE_KEY$NUMBER_OF_INVITATIONS", NUMBER_OF_INVITATIONS_DEFAULT_VALUE))
+                result = SpannableString(fromDictionary("$BASE_KEY$NUMBER_OF_INVITATIONS_TAIL", NUMBER_OF_INVITATIONS_TAIL_DEFAULT_VALUE))
             }
             connectionsRequestAccepted -> {
-                result = SpannableString(fromDictionary("${baseKey}_connectionRequestWasAccepted"))
+                result = SpannableString(fromDictionary("$BASE_KEY$CONNECTION_REQUEST_WAS_ACCEPTED", CONNECTION_REQUEST_WAS_ACCEPTED_DEFAULT_VALUE))
             }
             userWasRecommendedToYou -> {
-                result = SpannableString(fromDictionary("${baseKey}_userWasRecommendedToYou"))
+                val name = getRecommendedName(item)
+                val recomend = fromDictionary("$BASE_KEY$USER_WAS_RECOMENDED_TO_YOU", USER_WAS_RECOMENDED_TO_YOU_DEFAULT_VALUE)
+                val text = "$recomend $name"
+                result = getOneSpanText(text, name, Color.BLACK)
             }
             userWasRecommendedInEvent -> {
-                result = SpannableString(fromDictionary("${baseKey}_userWasRecommendedToYouInEvent"))
+                result = SpannableString(fromDictionary("$BASE_KEY$USER_WAS_RECOMENDED_TO_YOU_IN_EVENT", USER_WAS_RECOMENDED_TO_YOU_IN_EVENT_DEFAULT_VALUE))
+                result = SpannableString(fromDictionary("$BASE_KEY$USER_WAS_RECOMENDED_TO_YOU_IN_EVENT_2", USER_WAS_RECOMENDED_TO_YOU_IN_EVENT_2_DEFAULT_VALUE))
             }
             postPromoted -> {
-                result = SpannableString(fromDictionary("${baseKey}_$typeRawValue"))
+                result = SpannableString(fromDictionary("${BASE_KEY}_$typeRawValue", YOU_PROMOTED_YOUR_POST_DEFAULT_VALUE))
             }
             oneDayToExpirationOfPost -> {
-                result = SpannableString(fromDictionary("${baseKey}_$typeRawValue"))
+                result = SpannableString(fromDictionary("${BASE_KEY}_$typeRawValue", YOUR_POST_WILL_EXPIRE_TOMORROW_DEFAULT_VALUE))
             }
             postIsExpired -> {
-                result = SpannableString(fromDictionary("${baseKey}_$typeRawValue"))
+                result = SpannableString(fromDictionary("${BASE_KEY}_$typeRawValue", YOUR_POST_WAS_EXPIRED_DEFAULT_VALUE))
             }
             eventCancelling -> {
-                val canceled = fromDictionary("${baseKey}_eventWasCancelled")
-                val pointsReturns = fromDictionary("${baseKey}_eventWasCancelledPointsReturning")
+                val canceled = fromDictionary("$BASE_KEY$EVENT_WAS_CANCELLED", EVENT_WAS_CANCELLED_DEFAULT_VALUE)
+                val pointsReturns = fromDictionary("${BASE_KEY}$EVENT_WAS_CANCELLED_POINTS_RETURNING", EVENT_WAS_CANCELLED_POINTS_RETURNING_DEFAULT_VALUE)
                 val text = "${eventName ?: ""} $canceled ${totalPrice ?: ""} $pointsReturns"
                 result = getEventCancellingText(text, eventName ?: "", totalPrice
                         ?: "", Color.BLACK)
 
             }
+            else -> result = SpannableString(item.text)
         }
         return result
+    }
+
+    private fun getRecommendedName(item: NotificationModel): String {
+        val recommended = item.extra?.recommended ?: return ""
+        val name = if (recommended.accountType == AccountType.PERSONAL) {
+            "${recommended.personalInfo?.firstName} ${recommended.personalInfo?.lastName}"
+        } else {
+            recommended.organizationInfo?.organizationName
+        }
+        return requireNotNull(name)
     }
 
     private fun getEventCancellingText(text: String, eventName: String, pointsReturns: String, color: Int): SpannableString {
@@ -136,6 +159,13 @@ class NotificationHolder(itemView: View, private val onClickListener: View.OnCli
         return span
     }
 
+    private fun getOneSpanText(text: String, spanText: String, color: Int): SpannableString {
+        val span = SpannableString(text)
+        val pointsReturnsPosition = text.indexOf(spanText)
+        span.setSpan(ForegroundColorSpan(color), pointsReturnsPosition, pointsReturnsPosition + spanText.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        span.setSpan(StyleSpan(Typeface.BOLD), pointsReturnsPosition, pointsReturnsPosition + spanText.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        return span
+    }
 
     companion object {
         const val START_SPAN = 0
