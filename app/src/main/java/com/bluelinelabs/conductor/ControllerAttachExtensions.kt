af5729f76692c02b9e204f457be7d3ec0629e868
Peter <pilc@isddesign.com>
2018-04-20 09:51:09 +0300

    fix: removed unused code

--- app/src/main/java/com/bluelinelabs/conductor/ControllerAttachExtensions.kt
+++
@@ -1,9 +0,0 @@
-package com.bluelinelabs.conductor
-
-/**
- * Created by Peter on 4/17/2018.
- */
-//TODO: to delete
-fun Controller.setNeedsAttach(needsAttach: Boolean) {
-    this.needsAttach = needsAttach
-}
