27407bb59c0337e86c9b3121dd4fb21b4308eaea
Peter <pilc@isddesign.com>
2018-05-11 10:04:38 +0300

    refactor: simplified recommended connections logic

--- domain/src/main/java/com/mnassa/domain/model/RecommendedConnections.kt
+++ domain/src/main/java/com/mnassa/domain/model/RecommendedConnections.kt
@@ -6,9 +6,7 @@ import java.io.Serializable
  * Created by Peter on 11.03.2018.
  */
 interface RecommendedConnections : Serializable {
-    val byPhone: Map<String, List<ShortAccountModel>>
-    val byGroups: Map<String, List<ShortAccountModel>>
-    val byEvents: Map<String, List<ShortAccountModel>>
+    val recommendations: Map<String, List<ShortAccountModel>>
 
-    val isEmpty: Boolean get() = byPhone.isEmpty() && byGroups.isEmpty() && byEvents.isEmpty()
+    val isEmpty: Boolean get() = recommendations.isEmpty()
 }
