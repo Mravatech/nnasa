ee006a3ba33551126b6dcbdf23576718873af147
Peter <pilc@isddesign.com>
2018-09-07 15:29:30 +0300

    fix: removed logs

--- data/src/main/java/com/mnassa/data/extensions/LogExtensions.kt
+++ data/src/main/java/com/mnassa/data/extensions/LogExtensions.kt
@@ -6,6 +6,5 @@ import com.mnassa.data.BuildConfig
  * Created by Peter on 5/31/2018.
  */
 internal inline fun forDebug(crossinline func: () -> Unit) {
-    func()
     //if (BuildConfig.DEBUG) func()
 }
