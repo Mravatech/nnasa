41e1e33fbd783659877c5f027a583471cea594b1
Peter <pilc@isddesign.com>
2018-09-28 11:36:13 +0300

    fix: login

--- data/src/main/java/com/mnassa/data/network/bean/retrofit/response/CheckPhoneResponse.kt
+++ data/src/main/java/com/mnassa/data/network/bean/retrofit/response/CheckPhoneResponse.kt
@@ -6,7 +6,7 @@ import com.google.gson.annotations.SerializedName
  * Created by Peter on 9/25/2018.
  */
 data class CheckPhoneResponse(
-        @SerializedName("data") val data: CheckPhoneResponseData
+        @SerializedName("data") val data: CheckPhoneResponseData?
 ) : MnassaResponse()
 
 data class CheckPhoneResponseData(
