85df9d173b8d15c9087891f716f9271a349927f8
Artem Chepurnoy <artemchep@gmail.com>
2019-03-15 17:51:13 +0200

    Make ViewModel use a SupervisorJob scope

--- core/src/main/java/com/mnassa/core/BaseViewModelImpl.kt
+++ core/src/main/java/com/mnassa/core/BaseViewModelImpl.kt
@@ -5,11 +5,13 @@ import androidx.annotation.CallSuper
 import androidx.lifecycle.ViewModel
 import com.mnassa.core.addons.SubscriptionContainer
 import com.mnassa.core.addons.SubscriptionsContainerDelegate
+import kotlinx.coroutines.SupervisorJob
 
 /**
  * Created by Peter on 2/20/2018.
  */
-abstract class BaseViewModelImpl : ViewModel(), BaseViewModel, SubscriptionContainer by SubscriptionsContainerDelegate() {
+abstract class BaseViewModelImpl : ViewModel(), BaseViewModel,
+    SubscriptionContainer by SubscriptionsContainerDelegate(jobFactory = { SupervisorJob() }) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         openSubscriptionsScope()
